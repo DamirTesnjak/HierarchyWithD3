9d81b4cc1ff8902c2baf6c8a0bc89a1d8c37f5a7
DamirTesnjak <damir.tesjak@gmail.com>
2025-06-10 20:17:24 +0200

    feat: implementing event handlers, in progress

--- src/components/TreeView/TreeView.jsx
+++ src/components/TreeView/TreeView.jsx
@@ -1,107 +1,122 @@
-import { hierarchy, select } from "d3";
-import { useRef, useState, useEffect, useCallback } from "react";
+import { hierarchy, select, selectAll } from "d3";
+import { useRef, useState, useEffect, useCallback, useMemo } from "react";
 import { transformData } from "../../utils/transformData";
 import Toolbar from "../Toolbar";
 
 export function TreeView() {
-  const data = {
-    Hierarchy: [
-      {
-        Q3: [
-          {
-            Jul: 113.4,
-          },
-          {
-            Aug: 46.4,
-          },
-          {
-            Sep: 42.7,
-          },
-        ],
-      },
-      {
-        Q4: [
-          {
-            Oct: 115.5,
-          },
-          {
-            Nov: 24.8,
-          },
-          {
-            Dec: [
-              {
-                Oct: 115.5,
-              },
-              {
-                Nov: 24.8,
-              },
-              {
-                Dec: 97.2,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
+  const data = useMemo(
+    () => ({
+      Hierarchy: [
+        {
+          Q3: [
+            {
+              Jul: 113.4,
+            },
+            {
+              Aug: 46.4,
+            },
+            {
+              Sep: 42.7,
+            },
+          ],
+        },
+        {
+          Q4: [
+            {
+              Oct: 115.5,
+            },
+            {
+              Nov: 24.8,
+            },
+            {
+              Dec: [
+                {
+                  Oct: 115.5,
+                },
+                {
+                  Nov: 24.8,
+                },
+                {
+                  Dec: 97.2,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }),
+    []
+  );
 
   const [action, setAction] = useState({
     skip: false,
     invert: false,
   });
 
-  const drawHierarchicalStructure = useCallback((data, svg) => {
-    const nodeSize = 20;
-    const root = hierarchy(data).eachBefore(
-      // for each node and decendandt in pre-order traversal we add am index value,
-      // starting with the 0
-      ((i) => (d) => {
-        d.index = i++;
-        d.skip = false;
-        d.invert = false;
-        d.collapsed = false;
-      })(0)
-    );
-    const nodes = root.descendants();
-
-    console.log("nodes", nodes);
-
-    const width = 1000;
-    const height = (nodes.length + 2) * nodeSize;
-
-    svg
-      .attr("width", width)
-      .attr("height", height)
-      // .attr("viewBox", [-nodeSize / 2, (-nodeSize * 3) / 2, width, height])
-      .attr(
-        "style",
-        "max-width: 100%; height: auto; font: 15px sans-serif; overflow: visible;"
+  const drawHierarchicalStructure = useCallback(
+    (data, svg) => {
+      const nodeSize = 20;
+      const root = hierarchy(data).eachBefore(
+        // for each node and decendandt in pre-order traversal we add am index value,
+        // starting with the 0
+        ((i) => (d) => {
+          d.index = i++;
+          d.skip = false;
+          d.invert = false;
+          d.collapsed = false;
+        })(0)
       );
+      const nodes = root.descendants();
 
-    const node = svg
-      .selectAll("g")
-      .data(nodes)
-      .join("g")
-      .attr("transform", (d) => `translate(0,${d.index * nodeSize})`);
+      const width = 1000;
+      const height = (nodes.length + 2) * nodeSize;
 
-    node
-      .append("circle")
-      .attr("cx", (d) => nodeSize)
-      .attr("r", 2.5)
-      .attr("fill", (d) => (d.children ? null : "#999"));
+      svg
+        .attr("width", width)
+        .attr("height", height)
+        // .attr("viewBox", [-nodeSize / 2, (-nodeSize * 3) / 2, width, height])
+        .attr(
+          "style",
+          "max-width: 100%; height: auto; font: 15px sans-serif; overflow: visible;"
+        );
 
-    node // display label
-      .append("text")
-      .attr("dy", "0.32em")
-      .attr("x", (d) => d.depth * nodeSize + 6)
-      .text((d) => d.data.name);
+      const node = svg
+        .selectAll("g")
+        .data(nodes)
+        .join("g")
+        .attr("transform", (d) => `translate(0,${d.index * nodeSize})`)
+        .on("click", function (e, d) {
+          d.skip = action.skip;
+          d.invert = action.invert;
 
-    node // display value
-      .append("text")
-      .attr("dy", "0.32em")
-      .attr("x", (d) => (d.depth * nodeSize + 6) * 2)
-      .text((d) => getSumValueOfNode(d));
-  }, []);
+          select(this)
+            .selectAll("text")
+            .style("text-decoration", (d) => {
+              console.log("d", d);
+              return d.skip ? "line-through" : "none";
+            });
+        });
+
+      node
+        .append("circle")
+        .attr("cx", (d) => nodeSize)
+        .attr("r", 2.5)
+        .attr("fill", (d) => (d.children ? null : "#999"));
+
+      node // display label
+        .append("text")
+        .attr("dy", "0.32em")
+        .attr("x", (d) => d.depth * nodeSize + 6)
+        .text((d) => d.data.name);
+
+      node // display value
+        .append("text")
+        .attr("dy", "0.32em")
+        .attr("x", (d) => (d.depth * nodeSize + 6) * 2)
+        .text((d) => getSumValueOfNode(d));
+    },
+    [action.invert, action.skip]
+  );
 
   const svgRef = useRef();
 
@@ -109,7 +124,10 @@ export function TreeView() {
     const svg = select(svgRef.current);
 
     // Clear previous drawing
-    svg.selectAll("*").remove();
+
+    // -- TODO -
+    // logic to update only necessary elements not the entire tree!!!
+    // svg.selectAll("*").remove();
 
     // Example: draw a hierarchical structure
     const tranformedData = transformData(data);
@@ -117,6 +135,7 @@ export function TreeView() {
   }, [data, action, drawHierarchicalStructure]);
 
   function getSumValueOfNode(d) {
+    console.log("d", d);
     if (d.children) {
       d.sum((child) => child.value);
       return d.value;
@@ -136,6 +155,8 @@ export function TreeView() {
       action: () => setAction({ skip: false, invert: !action.invert }),
     },
   };
+
+  console.log("action", action);
 
   return (
     <div style={{ marginTop: 10 }}>
