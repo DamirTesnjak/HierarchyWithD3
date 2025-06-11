feed3818eea54bf6611f2f4eac583209569e74d6
DamirTesnjak <damir.tesjak@gmail.com>
2025-06-12 00:24:10 +0200

    feat: implementing event handlers, in progress

--- src/utils/getSumValueOfNode.js
+++ src/utils/getSumValueOfNode.js
@@ -3,19 +3,19 @@ export function getSumValueOfNode(d, invert = false) {
   if (d.children) {
     const childrenStoredValues = d
       .descendants()
-      .filter((d) => d.store)
+      .filter((d) => (invert ? d.store < 0 : d.store))
       .map((d) => d.store);
     console.log("childrenStoredValues", childrenStoredValues);
     const sumChildren = childrenStoredValues.reduce(
       (acc, curr) => acc + curr,
-      0
+      d.sumStore && invert ? d.sumStore : 0
     );
 
-    const sumOfSubsChildren = childrenStoredValues.reduce(
-      (acc, curr) => acc + (curr < 0 ? curr : 0),
-      sumChildren
-    );
-    return invert ? sumChildren : sumOfSubsChildren;
+    if (!invert) {
+      d.sumStore = sumChildren;
+    }
+
+    return sumChildren;
   }
   return d.data.value;
 }
